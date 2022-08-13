a18e54a8510d4bd7e6ac8c402c3c0d5bf029b88f
Sam M W <you@example.com>
2022-08-13 11:24:29 +0100

    start on the generate_test_case program

--- generate_test_case.c
+++ generate_test_case.c
@@ -8,12 +8,57 @@
 
 #define RNG "/dev/random"
 
+#include "solid65.h"
+#include "opcodes.h"
+
+int valid_opcode(char opcode) {
+	// This can be improved.
+	// (binary search would be much quicker since we know the array is sorted)
+	// Also it would be better not to depend on the size of the array
+	for(int i = 0; i < 152; i++) {
+		if(opcode == legals[i]) return 1;
+		if(opcode > legals[i]) return 0;
+	}
+	return 0;
+}
+
 int main() {
+
+	char testcase[TCLEN];
+
+	// Open the random number generator
 	FILE * rng = fopen(RNG, "r");
 	if(!rng) {
 		perror("couldn't open " RNG);
 		exit(1);
 	}
 
-	
+	// Read in the test case
+	for(int i = 0; i < TCLEN; i++) {
+		testcase[i] = fgetc(rng);
+	}
+
+	// Make sure that the opcode is a valid one
+	while(valid_opcode(testcase[PROG])) {
+		testcase[PROG] = fgetc(rng);
+	}
+
+	// This program doesn't make any claims about what the result should be, so
+	// clear out the result fields
+	testcase[A_PCH] = 0;
+	testcase[A_PCL] = 0;
+	testcase[A_A] = 0;
+	testcase[A_X] = 0;
+	testcase[A_Y] = 0;
+	testcase[A_P] = 0;
+	testcase[A_S] = 0;
+
+	// close the RNG
+	fclose(rng);
+
+	// Print out the generated test case
+	for(int i = 0; i < TCLEN; i++) {
+		printf("%02x ", (unsigned int) (testcase[i] & 0xff));
+	}
+	printf("\n");
 }
--- solid65.h
+++ solid65.h
@@ -1,1 +1,22 @@
 #define TCLEN 32
+
+// Set the CPU's internal state to these values before starting the test
+#define B_PCH 0
+#define B_PCL 1
+#define B_A   2
+#define B_X   3
+#define B_Y   4
+#define B_P   5
+#define B_S   6
+// Compare the CPU against these values after the test has run
+#define A_PCH 7
+#define A_PCL 8
+#define A_A   9
+#define A_X   10
+#define A_Y   11
+#define A_P   12
+#define A_S   13
+
+// Any time your emulated CPU reads a value, supply the values from this locus on.
+#define PROG  14
+
